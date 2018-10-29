9fa68cbee47b6af47eaffeb42688c70154994f24
Daniel Lopes <viana.daniel02@gmail.com>
2018-10-29 17:35:11 -0300

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+# conversor_moedas
+
+Aplicativo para conversão de moedas.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
+
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=55dd31be";
+
+void main() async{
+
+  http.Response response = await http.get(request);
+
+
+  runApp(MaterialApp(
+    home: Container()
+  ));
+}
