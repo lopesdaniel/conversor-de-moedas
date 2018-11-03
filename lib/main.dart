160b49f03e0b6877325db261879e04f74ef0ad7a
Daniel Lopes <viana.daniel02@gmail.com>
2018-11-02 23:16:53 -0300

    Criado interface da aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 
@@ -9,10 +9,100 @@ const request = "https://api.hgbrasil.com/finance?format=json&key=55dd31be";
 
 void main() async{
 
-  http.Response response = await http.get(request);
+
 
 
   runApp(MaterialApp(
-    home: Container()
+    home: Home(),
+    theme: ThemeData(
+      hintColor: Colors.amber,
+      primaryColor: Colors.white
+    ),
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+
+  return json.decode(response.body);
+}
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  double dolar;
+  double euro;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true
+      ),
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot){
+            switch(snapshot.connectionState){
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text("Carregando dados..",
+                  style: TextStyle(
+                    color: Colors.amber,
+                    fontSize: 25.0),
+                  textAlign: TextAlign.center)
+                );
+              default:
+                if(snapshot.hasError){
+                  return Center(
+                      child: Text("Erro ao carregar dados :(",
+                          style: TextStyle(
+                              color: Colors.amber,
+                              fontSize: 25.0),
+                          textAlign: TextAlign.center)
+                  );
+                } else {
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.monetization_on, size: 150.0, color: Colors.amber),
+                        buildTextField("Reais", "R\$"),
+                        Divider(),
+                        buildTextField("Dólares", "U\$"),
+                        Divider(),
+                          buildTextField("Euros", "€")
+                      ],
+                    ),
+                  );
+                }
+            }
+          }),
+    );
+  }
+}
+
+Widget buildTextField(String label, String prefix){
+  return TextField(
+    decoration: InputDecoration(
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.amber),
+        border: OutlineInputBorder(),
+        prefixText: prefix
+    ),
+    style: TextStyle(
+        color: Colors.amber, fontSize: 25.0
+    )
+  );
 }
